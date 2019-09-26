70f7ed4c75412d38e0d6f1677892a6ebcb19904d
chenxuanfeng <chenxuanfeng@meituan.com>
2019-09-26 09:43:20 +0800

    up

--- example/src/datepicker/index.vue
+++ example/src/datepicker/index.vue
@@ -12,19 +12,20 @@
             </div>
         </div>
     </div>
-    <Calendar v-if="buttonKeyNext == 'day'" lunar :value="lastDate" :disabled="disabledWeek" ></Calendar>
-    <Month v-if="buttonKeyNext == 'month'" :disabled="disabledWeek"></Month>
-    <Week v-if="buttonKeyNext == 'week'" :disabled="disabledWeek"></Week>
-    <Calendar v-if="buttonKeyNext == 'optional'" enable-range  lunar :value="lastDateRange" :disabled="disabledWeek"></Calendar>
-    <Quarter v-if="buttonKeyNext == 'quarter'" enable-range  lunar :year="lastQuarter.year" :quarter="lastQuarter.quarter"  :disabled="disabledWeek"></Quarter>
-    <Year v-if="buttonKeyNext == 'year'" enable-range  lunar :year="lastYear.year" :disabled="disabledYear"></Year>
-    <Holiday v-if="buttonKeyNext == 'festival'" enable-range  lunar :year="lastFestival.year" :title="lastFestival.title" :disabled="disabledWeek"></Holiday>
+    <Calendar v-if="buttonKeyNext == 'day'" v-bind="dayConfig" :disabled="disabledWeek" ></Calendar>
+    <Month v-if="buttonKeyNext == 'month'" v-bind="monthConfig" :disabled="disabledWeek"></Month>
+    <Week v-if="buttonKeyNext == 'week'" v-bind="weekConfig" :disabled="disabledWeek"></Week>
+    <Calendar v-if="buttonKeyNext == 'optional'" v-bind="optionalConfig" :disabled="disabledWeek"></Calendar>
+    <Quarter v-if="buttonKeyNext == 'quarter'" v-bind="quarterConfig"  :disabled="disabledWeek"></Quarter>
+    <Year v-if="buttonKeyNext == 'year'" v-bind="yearConfig" :disabled="disabledYear"></Year>
+    <Holiday v-if="buttonKeyNext == 'festival'" v-bind="festivalConfig" :disabled="disabledWeek"></Holiday>
   </div>
 </template>
 
 <script>
 import absoluteCalc from "./lib/absolute-calc";
 import naturalCalc from './lib/natural-date-calc';
+import {lastWeek, lastYearMonth, lastDate, lastDateRange, lastQuarter} from "./lib/default-config"
 import tools from "./lib/tools";
 import conf from "./lib/config";
 import common from "./lib/common";
@@ -42,14 +43,94 @@ import Holiday from "./holiday"
 import Calendar from "./calendar";
 import CommonHandle from "./commonhandle";
 
-const getLastDateRange = function() {
-  // 默认显示本周一到昨天
-  let startDate = tools.resetToFirst(new Date(), 'week'); // 重置周一
-  startDate = tools.dateFormat('Y/m/d', startDate);
-  let endDate = naturalCalc('-1day', new Date());
-  endDate = tools.dateFormat('Y/m/d', endDate);
-  return { startDate, endDate };
-}
+const holidayQuery = [
+    { name: '元旦', query: 'NewYearsDay' },
+    { name: '春节', query: 'SpringFestival' },
+    { name: '清明节', query: 'ChingMingFestival' },
+    { name: '劳动节', query: 'MayDay' },
+    { name: '端午节', query: 'DragonBoatFestival' },
+    { name: '中秋节', query: 'MoonFestival' },
+    { name: '国庆节', query: 'NationalHoliday' }
+];
+
+const handleDateChange = (value) => {
+    let item = {
+        name: '按日',
+        key: 'day',
+        navName: tools.dateFormat('m-d', value),
+        fetchDate: tools.dateFormat('Y-m-d', value)
+    };
+};
+
+const handleWeekChange = (value) => {
+    let year = new Date(value.sunday).getFullYear();
+    let monday = tools.dateFormat('Y-m-d', new Date(value.monday));
+    let sunday = tools.dateFormat('Y-m-d', new Date(value.sunday));
+    let item = {
+        name: '按周',
+        key: 'week',
+        navName: `W${value.week}`,
+        fetchDate: `${monday}~${sunday}`
+    };
+};
+
+const handleMonthChange = (month, year) => {
+    let newMonth = month < 10 ? `0${month}` : month;
+    let item = {
+        name: '按月',
+        key: 'month',
+        navName: `${year}-${newMonth}`,
+        fetchDate: `${year}-${newMonth}`
+    };
+};
+
+const handleRangeChange = (dateGroup, formatString) => {
+    // 选中单独一天无效
+    if (dateGroup.startDate - dateGroup.endDate === 0) {
+        return false;
+    }
+    let item = {
+        name: '自定义时间段',
+        key: 'optional',
+        navName: `${tools.dateFormat('m-d', dateGroup.startDate)}~${tools.dateFormat('m-d', dateGroup.endDate)}`,
+        fetchDate: `${tools.dateFormat('Y-m-d', dateGroup.startDate)}~${tools.dateFormat('Y-m-d', dateGroup.endDate)}`
+    };
+};
+
+const handleQuarterChange = (quarter, year) => {
+    if (quarter === '') {
+        maskAction.update(MIDS.HEAD_CALENDAR_MASK, false, {});
+        return;
+    }
+    let item = {
+        name: '',
+        key: 'quarter',
+        navName: `第${quarter}季度`,
+        fetchDate: `${year}-${quarter}`
+    };
+};
+
+const handleYearChange = (y) => {
+    let item = {
+        name: '',
+        key: 'year',
+        navName: `按年 ${y}年`,
+        fetchDate: y
+    };
+};
+
+const handleFestivalChange = (param, year) => {
+    let data = [];
+    data = holidayQuery.filter((item, i) => {
+        return item.name.indexOf(param.name) !== -1;
+    });
+    let item = {
+        name: '',
+        key: 'festival',
+        navName: `${param.name}`,
+        fetchDate: `${year}-${data[0].query}`
+    };
+};
 
 export default {
   name: "DatePicker",
@@ -68,6 +149,75 @@ export default {
     "common-handle": CommonHandle
   },
   props: {
+    dayConfig: {
+      type: Object,
+      default: function(){
+        return {
+          lunar : true,
+          onChange: handleDateChange,
+          value: lastDate()
+        }
+      }
+    },
+    weekConfig: {
+      type: Object,
+      default: function() {
+        return {
+          ...lastWeek(),
+          onChange: handleWeekChange,
+        }
+      }
+    },
+    monthConfig: {
+      type: Object,
+      onChange: handleMonthChange,
+      default: lastYearMonth
+    },
+    quarterConfig: {
+      type: Object,
+      default: function(){
+        return {
+          ...lastQuarter(),
+          enableRange: true,
+          lunar: true,
+          onChange: handleQuarterChange
+        }
+      }
+    },
+    yearConfig: {
+      type: Object,
+      default: function(){
+        return {
+          enableRange: true,
+          lunar: true,
+          year: tools.getYear(),
+          onChange: handleYearChange
+        }
+      }
+    },
+    festivalConfig: {
+      type: Object,
+      default: function(){
+        return {
+          enableRange: true,
+          lunar: true,
+          year: tools.getYear(),
+          title: '',
+          onChange: handleFestivalChange
+        }
+      }
+    },
+    optionalConfig: {
+      type: Object,
+      default: function(){
+        return {
+          enableRange: true,
+          lunar: true,
+          value: lastDateRange(),
+          onChange: handleRangeChange
+        }
+      }
+    },
     showKeyList: {
       type: Array,
       default: function(){ return ['day', 'week', 'month', 'quarter', 'year', 'festival', 'optional']}
@@ -79,20 +229,7 @@ export default {
   },
   data: function() {
     return {
-      buttonKeyNext: this.buttonKey,
-      lastQuarter: {
-          year: 2019,
-          quarter: 3
-      },
-      lastFestival: {
-        year: 2019,
-        title: "节假日"
-      },
-      lastYear: {
-        year: 2019
-      },
-      lastDate: naturalCalc('-1day', new Date()),
-      lastDateRange: getLastDateRange()
+      buttonKeyNext: this.buttonKey
     };
   },
   created() {
@@ -141,7 +278,7 @@ export default {
             yesterday = naturalCalc('+7day', yesterday);
         }
         date = naturalCalc('-0day', date);
-        // today = tool.buildNoTimesDate(yesterday);
+        // today = tools.buildNoTimesDate(yesterday);
         if (date < boundary || date > yesterday) {
             return true;
         }
@@ -156,7 +293,6 @@ export default {
         }
         return false;
     }
-
   }
 };
 </script>
